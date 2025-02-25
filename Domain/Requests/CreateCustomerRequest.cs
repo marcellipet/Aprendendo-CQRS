158d6c8e3cb15b61de1e67d30554e93629c4801e
marcellipet <marcellipetranel@gmail.com>
2025-02-25 14:22:13 -0300

    Request e Response

---
+++ Domain/Requests/CreateCustomerRequest.cs
@@ -0,0 +1,6 @@
+namespace Shop.Domain.Commands.Requests{
+    public class CreateCustumerRequest{
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Domain/Responses/CreateCustomerResponse.cs
+++ Domain/Responses/CreateCustomerResponse.cs
@@ -1,6 +1,8 @@
 namespace Shop.Domain.Commands.Requests{
     public class CreateCustumerResponse{
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public DateTime Date { get; set; }
     }
 }
